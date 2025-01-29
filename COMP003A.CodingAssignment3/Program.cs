6fdce4a3d7cdb28203ac3797d6da0b7a0163b161
Michael Baker <michaelbaker4@my.whccd.edu>
2025-01-29 14:23:14 -0800

    Add project files.

---
+++ COMP003A.CodingAssignment3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Author: Michael Baker
+//Course: COMP-003A
+//Faculty: Jonathan Cruz
+//Purpose: Budget management application demonstrating control flow
+
+
+namespace COMP003A.CodingAssignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your monthly income: ");
+
+        }
+    }
+}
