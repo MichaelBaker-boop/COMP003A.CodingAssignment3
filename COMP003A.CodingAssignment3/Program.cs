863517f0765beda4495f2313341dff214445fed0
Michael Baker <michaelbaker4@my.whccd.edu>
2025-01-29 14:27:28 -0800

    Added Menu

--- COMP003A.CodingAssignment3/Program.cs
+++ COMP003A.CodingAssignment3/Program.cs
@@ -4,14 +4,22 @@
 //Purpose: Budget management application demonstrating control flow
 
 
+using System.Runtime.InteropServices;
+
 namespace COMP003A.CodingAssignment3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int income;
             Console.Write("Enter your monthly income: ");
+            income = int.Parse(Console.ReadLine());
 
+            Console.WriteLine("1. Add an Expense.\n");
+            Console.WriteLine("2. View Expenses and Budget.\n");
+            Console.WriteLine("3. Remove an Expense.\n");
+            Console.WriteLine("4. Exit");
         }
     }
 }
