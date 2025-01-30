f16b65f764c3a96980e8063c0a9271909ab44763
Michael Baker <michaelbaker4@my.whccd.edu>
2025-01-29 18:59:17 -0800

    Added trycatch block to check for income > zero

--- COMP003A.CodingAssignment3/Program.cs
+++ COMP003A.CodingAssignment3/Program.cs
@@ -12,14 +12,39 @@ namespace COMP003A.CodingAssignment3
     {
         static void Main(string[] args)
         {
-            int income;
-            Console.Write("Enter your monthly income: ");
-            income = int.Parse(Console.ReadLine());
+            Console.WriteLine("Welcome to the Budget Management Tool!");
+            int income; // The value of the Users income
 
+            while (true)
+            {
+
+                //try-catch block to handle invalid input
+
+                try
+                {
+                    // Prompt user to input income
+                    Console.Write("Enter your monthly income: ");
+                    // Parse input to integer
+                    income = int.Parse(Console.ReadLine());
+                    // Check if number is positive
+                    if (income <= 0) throw new Exception("Number must be more than zero.");
+                    // Exit loop with valid input
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Invalid input: {ex.Message}\n Please try again.");
+                }
+            }
             Console.WriteLine("1. Add an Expense.\n");
             Console.WriteLine("2. View Expenses and Budget.\n");
             Console.WriteLine("3. Remove an Expense.\n");
             Console.WriteLine("4. Exit");
+
+            int menuChoice;
+            Console.Write("Enter your choice");
+            menuChoice = int.Parse(Console.ReadLine());
+            
         }
     }
 }
